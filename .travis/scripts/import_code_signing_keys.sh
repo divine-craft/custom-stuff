e0d9ac3acb8d1fed3eb2af8db8d14e9153275a82
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-24 00:24:13 +0300

    chore: configure Maven and Travis for Maven Central deployment

---
+++ .travis/scripts/import_code_signing_keys.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This should be called from repository root
+
+# Import encrypted code_signing.asc file
+openssl aes-256-cbc -K "$encrypted_0dccf756cfe1_key" -iv "$encrypted_0dccf756cfe1_iv" \
+-in ./.travis/gpg/code_signing.asc.enc -out ./.travis/gpg/code_signing.asc -d
+
+gpg --fast-import ./.travis/gpg/code_signing.asc
