8c01f96e47a89a7da0b445eb685bed33201d5485
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-29 21:42:52 +0300

    chore: use gpg2 for importing signing key

--- .travis/scripts/import_code_signing_keys.sh
+++ .travis/scripts/import_code_signing_keys.sh
@@ -6,4 +6,4 @@
 openssl aes-256-cbc -K "$encrypted_0dccf756cfe1_key" -iv "$encrypted_0dccf756cfe1_iv" \
 -in ./.travis/gpg/code_signing.asc.enc -out ./.travis/gpg/code_signing.asc -d
 
-gpg --fast-import ./.travis/gpg/code_signing.asc
+gpg2 --import ./.travis/gpg/code_signing.asc
