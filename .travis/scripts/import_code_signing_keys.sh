17463cdb6a65d537f26c0adcb9b8a3e6473c772e
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-29 22:24:41 +0300

    revert: use GPG2 in import_code_signing_keys.sh"
    Refs: 3b5a8a7b

--- .travis/scripts/import_code_signing_keys.sh
+++ .travis/scripts/import_code_signing_keys.sh
@@ -6,4 +6,4 @@
 openssl aes-256-cbc -K "$encrypted_0dccf756cfe1_key" -iv "$encrypted_0dccf756cfe1_iv" \
 -in ./.travis/gpg/code_signing.asc.enc -out ./.travis/gpg/code_signing.asc -d
 
-gpg --batch --import ./.travis/gpg/code_signing.asc
+gpg2 --batch --import ./.travis/gpg/code_signing.asc
