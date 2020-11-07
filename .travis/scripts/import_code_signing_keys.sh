2746bd739a29b3364d87b6fb407b812136719856
progrm_jarvis <gh@progrm-jarvis.ru>
2020-11-08 02:47:57 +0300

    build(CI): apply  alternative code signing strategy

--- .travis/scripts/import_code_signing_keys.sh
+++ .travis/scripts/import_code_signing_keys.sh
@@ -7,4 +7,5 @@
 openssl aes-256-cbc -K "$encrypted_0dccf756cfe1_key" -iv "$encrypted_0dccf756cfe1_iv" \
 -in ./.travis/gpg/code_signing.asc.enc -out ./.travis/gpg/code_signing.asc -d
 
-gpg --batch --import ./.travis/gpg/code_signing.asc
+gpg2 --alow-secret-key-import --batch --passphrase="${CODE_SIGNING_GPG_KEY_PASSPHRASE}" \
+--keyring "${TRAVIS_BUILD_DIR}/pubring.gpg" --no-default-keyring --import ./.travis/gpg/code_signing.asc
