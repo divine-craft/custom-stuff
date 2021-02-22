a4036a57de55456bdc61486f79e0681dfb8d0d14
Petr Portnov <gh@progrm-jarvis.ru>
2021-02-22 03:20:08 +0300

    feat: add new simpler RecipeManager

--- custom-stuff-api/src/main/java/ru/divinecraft/customstuff/api/service/CustomStuff.java
+++ custom-stuff-api/src/main/java/ru/divinecraft/customstuff/api/service/CustomStuff.java
@@ -5,6 +5,7 @@ import ru.divinecraft.customstuff.api.advancement.manager.AdvancementManager;
 import ru.divinecraft.customstuff.api.block.manager.CustomBlockManager;
 import ru.divinecraft.customstuff.api.inventory.manager.CustomInventoryManager;
 import ru.divinecraft.customstuff.api.item.manager.CustomItemManager;
+import ru.divinecraft.customstuff.api.recipe.manager.RecipeManager;
 import ru.divinecraft.customstuff.api.render.CustomBlockRenderer;
 
 public interface CustomStuff {
@@ -20,6 +21,9 @@ public interface CustomStuff {
 
     @Contract(pure = true)
     AdvancementManager advancementManager();
+
+    @Contract(pure = true)
+    RecipeManager recipeManager();
 
     @Contract(pure = true)
     CustomBlockRenderer blockRenderer();
--- custom-stuff-api/src/main/java/ru/divinecraft/customstuff/api/service/SimpleCustomStuff.java
+++ custom-stuff-api/src/main/java/ru/divinecraft/customstuff/api/service/SimpleCustomStuff.java
@@ -7,10 +7,12 @@ import lombok.Value;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
+import org.jetbrains.annotations.NotNull;
 import ru.divinecraft.customstuff.api.advancement.manager.AdvancementManager;
 import ru.divinecraft.customstuff.api.block.manager.CustomBlockManager;
 import ru.divinecraft.customstuff.api.inventory.manager.CustomInventoryManager;
 import ru.divinecraft.customstuff.api.item.manager.CustomItemManager;
+import ru.divinecraft.customstuff.api.recipe.manager.RecipeManager;
 import ru.divinecraft.customstuff.api.render.CustomBlockRenderer;
 
 @Value
@@ -23,15 +25,17 @@ import ru.divinecraft.customstuff.api.render.CustomBlockRenderer;
     @NonNull CustomItemManager itemManager;
     @NonNull CustomInventoryManager inventoryManager;
     @NonNull AdvancementManager advancementManager;
+    @NonNull RecipeManager recipeManager;
     @NonNull CustomBlockRenderer blockRenderer;
 
-    public static CustomStuff create(final @NonNull CustomBlockManager blockManager,
-                                     final @NonNull CustomItemManager itemManager,
-                                     final @NonNull CustomInventoryManager inventoryManager,
-                                     final @NonNull AdvancementManager advancementManager,
-                                     final @NonNull CustomBlockRenderer blockRenderer) {
+    public static @NotNull CustomStuff create(final @NonNull CustomBlockManager blockManager,
+                                              final @NonNull CustomItemManager itemManager,
+                                              final @NonNull CustomInventoryManager inventoryManager,
+                                              final @NonNull AdvancementManager advancementManager,
+                                              final @NonNull RecipeManager recipeManager,
+                                              final @NonNull CustomBlockRenderer blockRenderer) {
         return new SimpleCustomStuff(
-                blockManager, itemManager, inventoryManager, advancementManager, blockRenderer
+                blockManager, itemManager, inventoryManager, advancementManager, recipeManager, blockRenderer
         );
     }
 }
